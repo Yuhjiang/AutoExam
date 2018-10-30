b86fb986e744f7f0e22a6174b66895073d3938b2
yuhao <jiang.yuhao0809@gmail.com>
2018-10-30 22:18:36 +0800

    Demo Version

--- collection.py
+++ collection.py
@@ -1,9 +1,22 @@
+# -*- coding=utf-8 -*-
+"""
+Module:     collection
+Summary:    收集所有题库的题目，并保存到csv
+Author:     Yuhao Jiang
+Created:    2018/10/30
+Updated:    2018/10/30 Ver 1.0 收集所有数据
+"""
 import pandas as pd
 import xlrd
+import sys
 
 path = r'E:\中建三局\文件\题库\技术部题库20180105'
-columns = ['序号', '题目', '答案', '来源']
-
+columns = ['序号', '题目', '答案', '来源', '题型']
+pdata = pd.DataFrame(columns=columns)
+judge_answer = {
+    '√': 'T',
+    '×': 'F',
+}
 
 def read_excel_file(fpath):
     """
@@ -13,31 +26,37 @@ def read_excel_file(fpath):
     """
     data = xlrd.open_workbook(fpath)
 
-    # TODO  增加多个读取多个sheet的功能
-    table = data.sheets()[0]
-    nrows = table.nrows
-
     index = []
     questions = []
     answers = []
     sources = []
+    types = []
+    # TODO  增加多个读取多个sheet的功能
+    for table in data.sheets():
+        nrows = table.nrows
 
-    for i in range(1, nrows-1):
-        row = table.row_values(i)
-        index.append(row[0])
-        questions.append(row[1])
-        answers.append(row[2])
-        sources.append(row[4] + row[5])
+        for i in range(1, nrows-1):
+            row = table.row_values(i)
+            index.append(row[0])
+            questions.append(row[1])
+            if table.name == '判断题':
+                answers.append(judge_answer.get(row[2], ''))
+            else:
+                answers.append(row[2])
+            sources.append(row[4] + row[5])
+            types.append(table.name)
 
-    pData['序号'] = index
-    pData['题目'] = questions
-    pData['答案'] = answers
-    pData['来源'] = sources
+    pdata['序号'] = index
+    pdata['题目'] = questions
+    pdata['答案'] = answers
+    pdata['来源'] = sources
+    pdata['题型'] = types
 
 
-file = '\DGTJ08-61-2010基坑工程技术规范.xlsx'
-pData = pd.DataFrame(columns=columns)
-read_excel_file(path+file)
-sFile = 'collection.csv'
-print(pData)
-pData.to_csv(sFile, index=False)
+if __name__ == '__main__':
+    fpath = path + '\\' + sys.argv[1] + '.xlsx'
+    # fpath = path + '\\' + 'GB50202-2002建筑地基基础工程施工质量验收规范.xlsx'
+    read_excel_file(fpath)
+    string = sys.argv[1].replace(' ', '')
+    csv_name = string[:string.find('-', 1)+5]
+    pdata.to_csv(csv_name + '.csv')
--- random_select.py
+++ random_select.py
@@ -1,7 +1,35 @@
+# -*- coding=utf-8 -*-
+"""
+Module:     random_select
+Summary:    随机出题
+Author:     Yuhao Jiang
+Created:    2018/10/30
+Updated:    2018/10/30 Ver 1.0 在命令行随机出题
+Todo:       根据配置文件选择出题方式
+"""
 import pandas as pd
 import numpy as np
+import sys
 
 
 def random_select(fpath):
     data = pd.read_csv(fpath)
-    newdata = data.sample()
+
+    end = ''
+    while end != ':quit' or end != ':q':
+        row = data.sample(n=1)
+        index, number, question, answer, source, typen = row.iloc[0]
+        # 输出题目
+        print(question)
+        ans = input('输入答案：')
+        if ans == answer:
+            print('回答正确')
+        else:
+            print('回答错误')
+            print('正确答案：{}\n来源：{}'.format(answer, source))
+        end = input()
+
+
+if __name__ == '__main__':
+    fpath = sys.argv[1]
+    random_select('{}.csv'.format(fpath))
